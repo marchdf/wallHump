0df4f8bebbc55077c0030c13b88a86918c2494d5
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2017-11-09 09:20:37 -0700

    Plot scripts update.

---
+++ twod/plot_profiles.py
@@ -0,0 +1,395 @@
+#!/usr/bin/env python3
+
+# ========================================================================
+#
+# Imports
+#
+# ========================================================================
+import argparse
+import sys
+import os
+import glob
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import pandas as pd
+import yaml
+
+# ========================================================================
+#
+# Some defaults variables
+#
+# ========================================================================
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', serif='Times')
+cmap_med = ['#F15A60', '#7AC36A', '#5A9BD4', '#FAA75B',
+            '#9E67AB', '#CE7058', '#D77FB4', '#737373']
+cmap = ['#EE2E2F', '#008C48', '#185AA9', '#F47D23',
+        '#662C91', '#A21D21', '#B43894', '#010202']
+dashseq = [(None, None), [10, 5], [10, 4, 3, 4], [
+    3, 3], [10, 4, 3, 4, 3, 4], [3, 3], [3, 3]]
+markertype = ['s', 'd', 'o', 'p', 'h']
+
+
+# ========================================================================
+#
+# Function definitions
+#
+# ========================================================================
+def parse_ic(fname):
+    """Parse the Nalu yaml input file for the initial conditions"""
+    with open(fname, 'r') as stream:
+        try:
+            dat = yaml.load(stream)
+            u0 = float(dat['realms'][0]['initial_conditions']
+                       [0]['value']['velocity'][0])
+            rho0 = float(dat['realms'][0]['material_properties']
+                         ['specifications'][0]['value'])
+            mu = float(dat['realms'][0]['material_properties']
+                       ['specifications'][1]['value'])
+
+            return u0, rho0, mu
+
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+# ========================================================================
+#
+# Main
+#
+# ========================================================================
+if __name__ == '__main__':
+
+    # ========================================================================
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description='A simple plot tool')
+    parser.add_argument(
+        '-s', '--show', help='Show the plots', action='store_true')
+    args = parser.parse_args()
+
+    # ======================================================================
+    # Nalu data stuff
+    porder = 2
+    fdir = os.path.abspath('103x28')
+    # fdir = os.path.abspath('205x55')
+    # fdir = os.path.abspath('409x109')
+    # fdir = os.path.abspath('817x217')
+    # fdir = os.path.abspath('1633x433')
+    rdir = os.path.abspath(os.path.join(fdir, 'results_p{0:d}'.format(porder)))
+    yname = os.path.join(fdir, 'wallHump_p{0:d}.i'.format(porder))
+    u0, rho0, mu = parse_ic(yname)
+    chord = 420
+    nasa_cp_shift = -0.015
+
+    # ======================================================================
+    # NASA experiment
+    fname = os.path.join(os.path.abspath('../nasa_data'), 'profiles_exp.dat')
+    df = pd.read_csv(fname, comment='#')
+
+    xs = [0.65, 0.8, 1.0, 1.1, 1.2, 1.3, -2.14]
+    cnt = 0
+    for k, x in enumerate(xs):
+
+        subdf = df[df['x'] == x]
+        plt.figure(cnt)
+        cnt += 1
+        p = plt.plot(subdf['u'],
+                     subdf['y'],
+                     ls='None',
+                     lw=1,
+                     color=cmap[-1],
+                     marker=markertype[0],
+                     mec=cmap[-1],
+                     mfc=cmap[-1],
+                     ms=6,
+                     label='Exp. x={0:.2f}'.format(x))
+
+        plt.figure(cnt)
+        cnt += 1
+        p = plt.plot(subdf['uv'],
+                     subdf['y'],
+                     ls='None',
+                     lw=1,
+                     color=cmap[-1],
+                     marker=markertype[0],
+                     mec=cmap[-1],
+                     mfc=cmap[-1],
+                     ms=6,
+                     label='Exp. x={0:.2f}'.format(x))
+
+    # ======================================================================
+    # NASA CFL3D
+    fname = os.path.join(os.path.abspath('../nasa_data'), 'profiles_cfl3d.dat')
+    df = pd.read_csv(fname, comment='#')
+
+    xs = [0.65, 0.8, 1.0, 1.1, 1.2, 1.3, -2.14]
+    cnt = 0
+    for k, x in enumerate(xs):
+        subdf = df[np.fabs(df['x'] - x) < 1e-3]
+        plt.figure(cnt)
+        cnt += 1
+        p = plt.plot(subdf['u'],
+                     subdf['y'],
+                     ls='-',
+                     lw=2,
+                     color=cmap[0],
+                     label='CFL3D x={0:.2f}'.format(x))
+
+        plt.figure(cnt)
+        cnt += 1
+        p = plt.plot(subdf['uv'],
+                     subdf['y'],
+                     ls='-',
+                     lw=2,
+                     color=cmap[0],
+                     label='CFL3D x={0:.2f}'.format(x))
+
+    # ======================================================================
+    # Nalu output
+    fname = 'profiles.dat'
+    df = pd.read_csv(fname)
+
+    # First line
+    subdf = df[(df.nx == 205) & (df.order == 1)]
+
+    xs = [0.65, 0.8, 1.0, 1.1, 1.2, 1.3, -2.14]
+    cnt = 0
+    for k, x in enumerate(xs):
+
+        ssdf = subdf[np.fabs(subdf['x'] - x) < 1e-3]
+
+        plt.figure(cnt)
+        cnt += 1
+        p = plt.plot(ssdf['u'],
+                     ssdf['y'],
+                     ls='-',
+                     lw=2,
+                     color=cmap[1],
+                     label='Nalu x={0:.2f}'.format(x))
+        p[0].set_dashes(dashseq[1])
+
+        plt.figure(cnt)
+        cnt += 1
+        p = plt.plot(ssdf['uv'],
+                     ssdf['y'],
+                     ls='-',
+                     lw=2,
+                     color=cmap[1],
+                     label='Nalu x={0:.2f}'.format(x))
+        p[0].set_dashes(dashseq[1])
+
+    # First line
+    subdf = df[(df.nx == 205) & (df.order == 2)]
+
+    xs = [0.65, 0.8, 1.0, 1.1, 1.2, 1.3, -2.14]
+    cnt = 0
+    for k, x in enumerate(xs):
+
+        ssdf = subdf[np.fabs(subdf['x'] - x) < 1e-3]
+
+        plt.figure(cnt)
+        cnt += 1
+        p = plt.plot(ssdf['u'],
+                     ssdf['y'],
+                     ls='-',
+                     lw=2,
+                     color=cmap[2],
+                     label='Nalu x={0:.2f}'.format(x))
+        p[0].set_dashes(dashseq[2])
+
+        plt.figure(cnt)
+        cnt += 1
+        p = plt.plot(ssdf['uv'],
+                     ssdf['y'],
+                     ls='-',
+                     lw=2,
+                     color=cmap[2],
+                     label='Nalu x={0:.2f}'.format(x))
+        p[0].set_dashes(dashseq[2])
+
+    # ======================================================================
+    # Format the plots
+    plt.figure(0)
+    ax = plt.gca()
+    plt.xlabel(r"$u / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.5, 1.5])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('u_0.pdf', format='pdf')
+    plt.savefig('u_0.png', format='png')
+
+    plt.figure(1)
+    ax = plt.gca()
+    plt.xlabel(r"$u'v' / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.005, 0])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('uv_0.pdf', format='pdf')
+    plt.savefig('uv_0.png', format='png')
+
+    plt.figure(2)
+    ax = plt.gca()
+    plt.xlabel(r"$u / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.5, 1.5])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('u_1.pdf', format='pdf')
+    plt.savefig('u_1.png', format='png')
+
+    plt.figure(3)
+    ax = plt.gca()
+    plt.xlabel(r"$u'v' / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.04, 0])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('uv_1.pdf', format='pdf')
+    plt.savefig('uv_1.png', format='png')
+
+    plt.figure(4)
+    ax = plt.gca()
+    plt.xlabel(r"$u / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.5, 1.5])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('u_2.pdf', format='pdf')
+    plt.savefig('u_2.png', format='png')
+
+    plt.figure(5)
+    ax = plt.gca()
+    plt.xlabel(r"$u'v' / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.04, 0])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('uv_2.pdf', format='pdf')
+    plt.savefig('uv_2.png', format='png')
+
+    plt.figure(6)
+    ax = plt.gca()
+    plt.xlabel(r"$u / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.5, 1.5])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('u_3.pdf', format='pdf')
+    plt.savefig('u_3.png', format='png')
+
+    plt.figure(7)
+    ax = plt.gca()
+    plt.xlabel(r"$u'v' / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.04, 0])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('uv_3.pdf', format='pdf')
+    plt.savefig('uv_3.png', format='png')
+
+    plt.figure(8)
+    ax = plt.gca()
+    plt.xlabel(r"$u / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.5, 1.5])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('u_4.pdf', format='pdf')
+    plt.savefig('u_4.png', format='png')
+
+    plt.figure(9)
+    ax = plt.gca()
+    plt.xlabel(r"$u'v' / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.04, 0])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('uv_4.pdf', format='pdf')
+    plt.savefig('uv_4.png', format='png')
+
+    plt.figure(10)
+    ax = plt.gca()
+    plt.xlabel(r"$u / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.5, 1.5])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('u_5.pdf', format='pdf')
+    plt.savefig('u_5.png', format='png')
+
+    plt.figure(11)
+    ax = plt.gca()
+    plt.xlabel(r"$u'v' / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.04, 0])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('uv_5.pdf', format='pdf')
+    plt.savefig('uv_5.png', format='png')
+
+    plt.figure(12)
+    ax = plt.gca()
+    plt.xlabel(r"$u / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.5, 1.5])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('u_6.pdf', format='pdf')
+    plt.savefig('u_6.png', format='png')
+
+    plt.figure(13)
+    ax = plt.gca()
+    plt.xlabel(r"$u'v' / u_r$", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$y$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    ax.set_xlim([-0.04, 0])
+    ax.set_ylim([0, 0.2])
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('uv_6.pdf', format='pdf')
+    plt.savefig('uv_6.png', format='png')
--- twod/plot_wall.py
+++ twod/plot_wall.py
@@ -14,9 +14,7 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 import yaml
-from netCDF4 import Dataset, chartostring
 
 # ========================================================================
 #
@@ -39,145 +37,6 @@ markertype = ['s', 'd', 'o', 'p', 'h']
 # Function definitions
 #
 # ========================================================================
-def parse_ic(fname):
-    """Parse the Nalu yaml input file for the initial conditions"""
-    with open(fname, 'r') as stream:
-        try:
-            dat = yaml.load(stream)
-            u0 = float(dat['realms'][0]['initial_conditions']
-                       [0]['value']['velocity'][0])
-            rho0 = float(dat['realms'][0]['material_properties']
-                         ['specifications'][0]['value'])
-            mu = float(dat['realms'][0]['material_properties']
-                       ['specifications'][1]['value'])
-
-            return u0, rho0, mu
-
-        except yaml.YAMLError as exc:
-            print(exc)
-
-
-# ========================================================================
-def parse_velocity_probe(rdir, uref):
-    """Read a Nalu velocity probe file."""
-
-    fnames = glob.glob(os.path.join(rdir, 'probe_profile*.dat'))
-
-    lst = []
-    for fname in fnames:
-
-        df = pd.read_csv(fname, delim_whitespace=True)
-        df.rename(columns={'Time': 'time',
-                           'coordinates[0]': 'x',
-                           'coordinates[1]': 'y',
-                           'velocity_probe[0]': 'u',
-                           'velocity_probe[1]': 'v',
-                           'reynolds_stress_probe[0]': 'uu',
-                           'reynolds_stress_probe[1]': 'uv',
-                           'reynolds_stress_probe[2]': 'vv'},
-                  inplace=True)
-        lst.append(df)
-    df = pd.concat(lst)
-
-    # Keep only the last time step
-    time = np.unique(df['time'])
-    df = df.loc[df['time'] == time[-1]]
-
-    # Non-dimensionalize some quantities
-    df['u'] = df['u'] / uref
-    df['v'] = df['v'] / uref
-    df['uu'] = df['uu'] / uref**2
-    df['uv'] = df['uv'] / uref**2
-    df['vv'] = df['vv'] / uref**2
-    return df.reset_index(drop=True)
-
-
-# ========================================================================
-def parse_flat_wall_probe(fname, yname):
-    """Read a Nalu wall probe file."""
-
-    df = pd.read_csv(fname, delim_whitespace=True)
-    df.rename(columns={'Time': 'time',
-                       'coordinates[0]': 'x',
-                       'coordinates[1]': 'y',
-                       'tau_wall_probe[0]': 'tau_wall',
-                       'pressure_probe[0]': 'pressure'},
-              inplace=True)
-
-    # Keep only the last time step
-    time = np.unique(df['time'])
-    df = df.loc[df['time'] == time[-1]]
-
-    # Calculate coefficients
-    u0, rho0, mu = parse_ic(yname)
-    dynPres = rho0 * 0.5 * u0 * u0
-    df['cf'] = df['tau_wall'] / dynPres
-    df['cp'] = df['pressure'] / dynPres
-
-    return df.reset_index(drop=True)
-
-
-# ========================================================================
-def parse_surface_probe(fname, yname):
-    """Read a Nalu exodus file to get surface data."""
-
-    wall_bdy = 'bottomwall'
-    tauwall_field = 'tau_wall'
-    pressure_field = 'pressure'
-
-    # Indices of wall face in element
-    ss_node_ids = np.array([0, 1, 2, 3])
-
-    # Read in the Exodus II mesh
-    msh = Dataset(fname, 'r')
-    ss_names = get_name_list(msh, "ss_names")
-    field_names = get_name_list(msh, "name_nod_var")
-    wall_idx = ss_names.index(wall_bdy)
-    tau_idx = field_names.index(tauwall_field)
-    pressure_idx = field_names.index(pressure_field)
-
-    # Get the coordinates and time
-    x = msh.variables['coordx'][:]
-    y = msh.variables['coordy'][:]
-    time = msh.variables['time_whole'][1:]
-
-    # Element mapping and wall node ids
-    nids = msh.variables['connect1'][:]
-    wall_elems = msh.variables['elem_ss%d' % (wall_idx + 1)][:] - 1
-    wall_nids_all = np.unique(
-        nids[np.ix_(wall_elems, ss_node_ids)].flatten()) - 1
-
-    # Get tau_wall and pressure on the wall
-    tau_wall_all = msh.variables['vals_nod_var%d' %
-                                 (tau_idx + 1)][:][1:, wall_nids_all]
-    pressure_all = msh.variables['vals_nod_var%d' %
-                                 (pressure_idx + 1)][:][1:, wall_nids_all]
-
-    # Keep only the last time step in a dataframe
-    df = pd.DataFrame()
-    df['tau_wall'] = tau_wall_all[-1, :]
-    df['pressure'] = pressure_all[-1, :]
-    df['time'] = time[-1]
-    df['x'] = x[wall_nids_all]
-
-    # Calculate coefficients
-    u0, rho0, mu = parse_ic(yname)
-    dynPres = rho0 * 0.5 * u0 * u0
-    df['cf'] = df['tau_wall'] / dynPres
-    df['cp'] = df['pressure'] / dynPres
-
-    return df
-
-
-# ========================================================================
-def get_name_list(msh, varname):
-    """Return a list of python strings converted from Exodus char format
-
-    Args:
-        msh (Dataset): A netCDF4 dataset instance
-        varname (str): A string instance
-    """
-    return [str(chartostring(v)) for v in msh.variables[varname]]
 
 
 # ========================================================================
@@ -197,16 +56,6 @@ if __name__ == '__main__':
 
     # ======================================================================
     # Nalu data stuff
-    porder = 2
-    fdir = os.path.abspath('103x28')
-    # fdir = os.path.abspath('205x55')
-    # fdir = os.path.abspath('409x109')
-    # fdir = os.path.abspath('817x217')
-    # fdir = os.path.abspath('1633x433')
-    rdir = os.path.abspath(os.path.join(fdir, 'results_p{0:d}'.format(porder)))
-    yname = os.path.join(fdir, 'wallHump_p{0:d}.i'.format(porder))
-    u0, rho0, mu = parse_ic(yname)
-    chord = 420
     nasa_cp_shift = -0.065
 
     # ======================================================================
@@ -234,8 +83,8 @@ if __name__ == '__main__':
                  lw=2,
                  label='CFL3D')
 
-    # # ======================================================================
-    # # Nalu output
+    # ======================================================================
+    # Nalu output
     fname = 'wall_vars.dat'
     df = pd.read_csv(fname)
 
@@ -247,6 +96,7 @@ if __name__ == '__main__':
                  color=cmap[1],
                  lw=2,
                  label='Nalu p=1')
+    p[0].set_dashes(dashseq[1])
 
     plt.figure(1)
     p = plt.plot(subdf['x'],
@@ -254,7 +104,9 @@ if __name__ == '__main__':
                  color=cmap[1],
                  lw=2,
                  label='Nalu p=1')
+    p[0].set_dashes(dashseq[1])
 
+    # Second line
     subdf = df[(df.nx == 103) & (df.order == 2)]
     subdf = subdf.reset_index()
     subdf = subdf.iloc[::2, :]
@@ -265,6 +117,7 @@ if __name__ == '__main__':
                  color=cmap[2],
                  lw=2,
                  label='Nalu p=2')
+    p[0].set_dashes(dashseq[2])
 
     plt.figure(1)
     p = plt.plot(subdf['x'],
@@ -272,6 +125,7 @@ if __name__ == '__main__':
                  color=cmap[2],
                  lw=2,
                  label='Nalu p=2')
+    p[0].set_dashes(dashseq[2])
 
     # ======================================================================
     # NASA experiment
