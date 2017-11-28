d5ec958f26075d70d90807491e731ac7f0865aab
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2017-11-28 10:34:31 -0700

    Added plotting scripts.

---
+++ twod/plot_coeffs.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+# ========================================================================
+#
+# Imports
+#
+# ========================================================================
+import argparse
+import sys
+import os
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# ========================================================================
+#
+# Some defaults variables
+#
+# ========================================================================
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', serif='Times')
+cmap_med = ['#F15A60', '#7AC36A', '#5A9BD4', '#FAA75B',
+            '#9E67AB', '#CE7058', '#D77FB4', '#737373']
+cmap = ['#EE2E2F', '#008C48', '#185AA9', '#F47D23',
+        '#662C91', '#A21D21', '#B43894', '#010202']
+dashseq = [(None, None), [10, 5], [10, 4, 3, 4], [
+    3, 3], [10, 4, 3, 4, 3, 4], [3, 3], [3, 3]]
+markertype = ['s', 'd', 'o', 'p', 'h']
+
+
+# ========================================================================
+#
+# Main
+#
+# ========================================================================
+if __name__ == '__main__':
+
+    # ========================================================================
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description='A simple plot tool')
+    parser.add_argument(
+        '-s', '--show', help='Show the plots', action='store_true')
+    args = parser.parse_args()
+
+    # ======================================================================
+    # Nalu data
+    sname = 'points.dat'
+    points_df = pd.read_csv(sname)
+    print(points_df)
+
+    # Drag
+    plt.figure(0)
+    subdf = points_df[points_df.order == 1]
+    p = plt.semilogx(subdf['dofs'],
+                     subdf['cd'],
+                     lw=2,
+                     color=cmap[0],
+                     marker=markertype[0],
+                     mec=cmap[0],
+                     mfc=cmap[0],
+                     ms=10,
+                     label='Nalu p=1')
+
+    subdf = points_df[points_df.order == 2]
+    p = plt.semilogx(subdf['dofs'],
+                     subdf['cd'],
+                     lw=2,
+                     color=cmap[1],
+                     marker=markertype[1],
+                     mec=cmap[1],
+                     mfc=cmap[1],
+                     ms=10,
+                     label='Nalu p=2')
+
+    # Lift
+    plt.figure(1)
+    subdf = points_df[points_df.order == 1]
+    p = plt.semilogx(subdf['dofs'],
+                     subdf['cl'],
+                     lw=2,
+                     color=cmap[0],
+                     marker=markertype[0],
+                     mec=cmap[0],
+                     mfc=cmap[0],
+                     ms=10,
+                     label='Nalu p=1')
+
+    subdf = points_df[points_df.order == 2]
+    p = plt.semilogx(subdf['dofs'],
+                     subdf['cl'],
+                     lw=2,
+                     color=cmap[1],
+                     marker=markertype[1],
+                     mec=cmap[1],
+                     mfc=cmap[1],
+                     ms=10,
+                     label='Nalu p=2')
+
+    # Format plots
+    plt.figure(0)
+    ax = plt.gca()
+    plt.xlabel(r"DoF", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$C_d$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    plt.savefig('cd.pdf', format='pdf')
+    plt.savefig('cd.png', format='png')
+
+    plt.figure(1)
+    ax = plt.gca()
+    plt.xlabel(r"DoF", fontsize=22, fontweight='bold')
+    plt.ylabel(r"$C_l$", fontsize=22, fontweight='bold')
+    plt.setp(ax.get_xmajorticklabels(), fontsize=18, fontweight='bold')
+    plt.setp(ax.get_ymajorticklabels(), fontsize=18, fontweight='bold')
+    legend = ax.legend(loc='best')
+    plt.tight_layout()
+    plt.savefig('cl.pdf', format='pdf')
+    plt.savefig('cl.png', format='png')
